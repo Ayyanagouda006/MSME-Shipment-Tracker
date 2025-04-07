7010abf088bf3636e84885ceec39bd3722f03d66
Ayyanagouda006 <ayyanagouda.r@agraga.com>
2025-04-07 16:19:41 +0530

    added cronjob

--- crojob.py
+++ crojob.py
@@ -2,16 +2,15 @@ import time
 import subprocess
  
 def run_script():
-    # Replace with the full path to your script
-	script_path = "/MSME-Shipment-Tracker/Backend_data.py"
-    
+    # Replace with the full path to your script   
     try:
-		subprocess.run(["python3", script_path], check=True)
+        script_path = "/MSME-Shipment-Tracker/Backend_data.py"
+        subprocess.run(["python3", script_path], check=True)
         print(f"Executed {script_path} successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error executing script: {e}")
  
-if __name__ == "__main__":
-    while True:
-        run_script()
-        time.sleep(3600) 
+
+while True:
+    run_script()
+    time.sleep(3600) 
